df542672675f2468fa43c4059a36a413c636bd62
Adriel Rober Freitas Fernandes <adriel.roberfreitas@outlook.com>
2024-07-08 09:38:03 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,8 @@ public class Program
 {
 	public static void main(String[] args) 
 	{
-		Department obj = new Department(1, "books");
-		System.out.println(obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		Seller seller =sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
